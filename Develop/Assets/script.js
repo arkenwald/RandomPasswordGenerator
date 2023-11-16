92dc281ad17f94fba7d86cf267d30b01e74b125c
arkenwald <149994852+arkenwald@users.noreply.github.com>
2023-11-16 16:01:00 -0500

    updated comments in script.js to show work

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,30 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Placeholder text displayed in display box
 document.getElementById(placeholder).value = password;
 
-// generate password
+// Generating the Password
 function generatePassword() {
-  //your code goes here
 
   var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
 
-  //ask for character type
+  //Prompting for character type
   var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
 
-  //evaluate character type
+  //Defining character types
   var charSet = "";
   var charTypeLower = charType.toLowerCase();
   if (charTypeLower === "lowercase") {
@@ -36,15 +34,15 @@ function generatePassword() {
   } else if (charTypeLower === "special") {
     charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   }
-  //return value
+  //Return value of generated password at random
   var retVal = "";
   for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
+    //Chooses the character at random from the index of the array
     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
   return retVal;
 
 }
 
-//make password appear in display box
+//Displaying the Generated Password within the Display Box
 document.getElementById("password").value = password;
